8466ee191b89e830c307ed0609e0ddcea0576bbf
erik verschueren <erik.verschueren@gmail.com>
2014-06-16 13:24:03 -0700

    deleted test file from master

--- tests/test_main.R
+++
@@ -1,7 +0,0 @@
-library(testthat)
-DEBUG=T
-source('main.R')
-
-config_small = 'tests/small/mist_small_test.yml'
-parsedArgs$config_file = config_small
-main(parsedArgs)
--- tests/test_preprocess.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-
-config = yaml.load(string=paste(readLines("tests/APMS_TEST.yml"),collapse='\n'))
-config = yaml.load(string=paste(readLines("tests/entero/APMS_ENTERO.yml"),collapse='\n'))
